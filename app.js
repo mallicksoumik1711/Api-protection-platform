4e7f0907c4a5e61cf452f4b230363e26feb43541
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-01-07 21:15:37 +0530

    remove redundency

--- app.js
+++ app.js
@@ -11,22 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 const databaseConnection = require("./config/db.config");
 databaseConnection();
 
-// const apiKeysAuth = require("./middleware/apiKeysAuth");
-// const rateLimit = require("./middleware/rateLimiter");
 const dummyData = require("./routes/dummyData.route");
 const authRoutes = require("./routes/auth.route");
-// const authMiddleware = require("./middleware/auth.middleware");
 
 const gateway = require("./gateway/gateway");
 app.use(gateway);
-
-app.get("/test", (req, res) => {
-  res.send("Working fine");
-});
-
-// app.use("/api", apiKeysAuth, rateLimit, authMiddleware, dummyData); //actuall correct order for protected routes, only for users who are logged in or validated
-// app.use("/unprotected-api", apiKeysAuth, rateLimit, dummyData); //for unprotected routes, only api key validation needed
-// app.use("/auth", rateLimit, authRoutes);
 
 app.use("/api", dummyData);
 app.use("/unprotected-api", dummyData);
