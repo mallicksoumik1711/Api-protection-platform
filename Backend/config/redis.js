97925f8a1650c2bc85f5b187dd001402f38fa101
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-03-31 17:35:54 +0530

    Update redis for prod connection

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -1,7 +1,24 @@
 const redis = require("redis");
-const client = redis.createClient();
-client.on("error", (err)=>{
-    console.log("Redis Client Error", err);
-})
-client.connect();
+const redisURL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+const client = redis.createClient({
+  url: redisURL,
+  // socket: {
+  //     host: "127.0.0.1",
+  //     port: 6379,
+  // },
+});
+client.on("connect", () => {
+  console.log("redis connected");
+});
+client.on("error", (err) => {
+  console.log("Redis Client Error", err);
+});
+(async () => {
+  try {
+    await client.connect();
+  } catch (err) {
+    console.log("Redis connection error: ", err);
+  }
+})();
+// client.connect();
 module.exports = client;
