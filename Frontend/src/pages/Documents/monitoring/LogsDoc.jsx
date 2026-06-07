8c6340b6a8eabf39aa616bc86703ba719d64ec6e
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-06-07 21:28:04 +0530

    Update logs doc headers

--- Frontend/src/pages/Documents/monitoring/LogsDoc.jsx
+++ Frontend/src/pages/Documents/monitoring/LogsDoc.jsx
@@ -15,10 +15,10 @@ function LogsDoc() {
       >
         <div className="max-w-6xl mx-auto px-4 sm:px-6 py-4">
           <DashboardHeader
-            tag={DocsHeaderValues.docsJwt.tag}
-            title={DocsHeaderValues.docsJwt.title}
-            description={DocsHeaderValues.docsJwt.description}
-            features={DocsHeaderValues.docsJwt.features}
+            tag={DocsHeaderValues.docsLogs.tag}
+            title={DocsHeaderValues.docsLogs.title}
+            description={DocsHeaderValues.docsLogs.description}
+            features={DocsHeaderValues.docsLogs.features}
           />
 
           <DocsLogs />
