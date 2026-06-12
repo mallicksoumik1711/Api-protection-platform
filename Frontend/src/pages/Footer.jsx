f4203464dbc12cebd5e3c14d96c8c9d8c168449d
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-06-12 18:27:21 +0530

    Adding footer Texts

--- Frontend/src/pages/Footer.jsx
+++ Frontend/src/pages/Footer.jsx
@@ -3,25 +3,33 @@ import { FaSquareXTwitter, FaSquareGithub, FaLinkedin } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <footer className="relative z-10 bg-gradient-to-br from-black via-slate-900 to-black text-white py-10 px-4 sm:px-6 w-full md:w-3/4 mx-auto sm:rounded-t-xl">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <div className="text-sm font-medium text-white">
-          <p>All Rights Reserved © 2026</p>
+    <footer className="relative overflow-hidden sm:rounded-t-xl mx-auto w-full md:w-3/4 bg-gradient-to-br from-black via-slate-900 to-black py-4">
+      <div className="relative z-10 flex flex-col justify-between">
+        {/* Logo Text */}
+        <div className="px-6 flex items-center justify-center sm:inline">
+          <h1 className="text-[4rem] sm:text-[8rem] font-black tracking-tight leading-none text-transparent bg-clip-text bg-gradient-to-br from-black/70 via-slate-900 to-black/30">
+            BOUNCER
+          </h1>
         </div>
-        <div className="flex flex-col md:flex-row justify-between items-center gap-8 sm:gap-10">
-          <div className="flex justify-center md:justify-end items-center gap-5 sm:gap-6 text-white/70 w-full md:w-auto">
-            <FaSquareXTwitter
-              size={18}
-              className="hover:text-white cursor-pointer transition h-6 w-6"
-            />
-            <FaSquareGithub
-              size={18}
-              className="hover:text-white cursor-pointer transition h-6 w-6"
-            />
-            <FaLinkedin
-              size={18}
-              className="hover:text-white cursor-pointer transition h-6 w-6"
-            />
+
+        {/* Bottom Bar */}
+        <div className="flex flex-col md:flex-row items-center justify-between px-6 md:px-10 gap-4 mt-8">
+          <p className="text-xs text-zinc-500">
+            © 2026 Bouncer. All rights reserved.
+          </p>
+
+          <div className="flex items-center gap-6 text-zinc-500">
+            <a href="#" className="hover:text-white transition text-sm">
+              LinkedIn
+            </a>
+
+            <a href="#" className="hover:text-white transition text-sm">
+              GitHub
+            </a>
+
+            <a href="#" className="hover:text-white transition text-sm">
+              Twitter
+            </a>
           </div>
         </div>
       </div>
