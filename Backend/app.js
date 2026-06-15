ecf5127a7842f9c40eb80f0a2f2e44d1e37c49dc
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-06-15 18:42:17 +0530

    update localhost to prod url

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5174", "https://bouncerapp.vercel.app"],
     credentials: true,
   }),
 );
