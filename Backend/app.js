bfb94e14ae03c7157050ea8896561bdce1962765
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-06-16 14:22:33 +0530

    Change cors url

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "https://bouncerapp.vercel.app"],
+    origin: ["http://localhost:5173", "https://bouncerapp.vercel.app"],
     credentials: true,
   }),
 );
